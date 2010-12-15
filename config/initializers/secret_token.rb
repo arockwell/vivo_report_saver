b9c17753bcbb4234878e3b881271e6fc1703ee02
Alex Rockwell <alexhr@ufl.edu>
2010-12-15 11:25:52 -0500

    Initial commit of blank rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VivoReportSaver::Application.config.secret_token = 'e3535cce636956cadc7b8d21095c36883ac21c669c3b5ffb03b8a959eabffe40e32f1643675303f17e2bd158cf70c557b2a2f76257d01a314c98bbf3a95fe246'
